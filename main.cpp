8db3f444455d52d5ae8c5a1dbf62ec9deda98cfd
James Smith <jmsmith86@gmail.com>
2022-04-16 22:04:28 -0600

    Use alternating buffers to reduce transmission time with large packets

--- main.cpp
+++ main.cpp
@@ -19,80 +19,158 @@ const uint SDCKB_PIN = 15;
 const uint SDCKA_MASK = (1 << SDCKA_PIN);
 const uint SDCKB_MASK = (1 << SDCKB_PIN);
 
-// The size of this doesn't represent bits but rather max number of state changes
-// This should be able to handle a minumum of 31 x 32-bit words plus crc byte
-volatile uint32_t states[2048];
-volatile uint32_t* volatile currentState = states;
-volatile uint32_t numStates = 0;
+enum Bank
+{
+    BANK_A = 0,
+    BANK_B,
 
+    BANK_COUNT
+};
+
+// The size of this doesn't represent bits but rather max number of state changes
+#define ELEMENTS_PER_BANK 256
+
+class DataBank
+{
+    public:
+        DataBank() :
+            mData(),
+            mCurrent(mData),
+            mEnd(mData),
+            mCapacityEnd(mData + ELEMENTS_PER_BANK)
+        {}
+
+        inline void reset() volatile
+        {
+            mEnd = mData;
+            mCurrent = mData;
+        }
+
+        inline void write(uint32_t datum) volatile
+        {
+            *mEnd++ = datum;
+        }
+
+        inline uint32_t read() volatile
+        {
+            return *mCurrent++;
+        }
+
+        inline int32_t remainingCapacity() volatile
+        {
+            return (mCapacityEnd - mEnd);
+        }
+
+        inline bool empty() volatile
+        {
+            return (mEnd == mCurrent);
+        }
+
+    private:
+        volatile uint32_t mData[ELEMENTS_PER_BANK];
+        volatile uint32_t* volatile mCurrent;
+        volatile uint32_t* volatile mEnd;
+        volatile uint32_t* volatile const mCapacityEnd;
+};
+
+volatile DataBank g_dataBanks[BANK_COUNT];
+volatile DataBank* g_currentBank = &g_dataBanks[BANK_A];
+
+extern "C" {
 void isr_systick(void)
 {
-    if (numStates > 0)
+    if (!g_currentBank->empty())
     {
-        gpio_put_all(*currentState++);
-        --numStates;
+        gpio_put_all(g_currentBank->read());
     }
     
-    if (numStates == 0)
+    if (g_currentBank->empty())
     {
         // All done
         systick_hw->csr = 0;
     }
 }
-
-static inline void reset(void)
-{
-    numStates = 0;
-    currentState = states;
 }
 
-static inline volatile uint32_t* flush(volatile uint32_t* ptr)
+static inline void reset_all(void)
 {
-    if (ptr != NULL)
+    for (uint32_t i = BANK_A; i < BANK_COUNT; ++i)
     {
-        numStates = ptr - currentState;
+        g_dataBanks[i].reset();
     }
+    g_currentBank = &g_dataBanks[BANK_A];
+}
 
-    if (numStates > 0)
+static inline void flush()
+{
+    if (!(systick_hw->csr & SYST_CSR_ENABLE_MASK))
     {
         systick_hw->csr |= (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_TICKINT_MASK | SYST_CSR_ENABLE_MASK);
         systick_hw->cvr = 0;
-        while (systick_hw->csr & SYST_CSR_ENABLE_MASK);
     }
-    else
+    while (systick_hw->csr & SYST_CSR_ENABLE_MASK);
+    g_currentBank->reset();
+}
+
+static inline Bank send(Bank bank)
+{
+    if (&g_dataBanks[bank] != g_currentBank)
     {
-        systick_hw->csr = 0;
+        if (!g_currentBank->empty())
+        {
+            flush();
+        }
+        g_currentBank = &g_dataBanks[bank];
     }
 
-    reset();
+    if (!(systick_hw->csr & SYST_CSR_ENABLE_MASK))
+    {
+        systick_hw->csr |= (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_TICKINT_MASK | SYST_CSR_ENABLE_MASK);
+        systick_hw->cvr = 0;
+    }
 
-    return currentState;
+    bank = static_cast<Bank>(static_cast<uint32_t>(bank) + 1);
+    if (bank >= BANK_COUNT)
+    {
+        bank = BANK_A;
+    }
+    return bank;
 }
 
 void write(uint8_t* bytes, uint32_t len)
 {
-    volatile uint32_t* nextState = flush(NULL);
+    reset_all();
+    Bank bank = BANK_A;
+    volatile DataBank* dataBank = &g_dataBanks[bank];
 
     // Ensure it's at neutral for a few cycles
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
 
     // Start
-    *nextState++ = SDCKB_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKB_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKB_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKB_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKB_MASK;
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKB_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKB_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKB_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKB_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKB_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
 
     // Data
+    uint32_t lastState = (SDCKA_MASK | SDCKB_MASK);
     for (uint32_t i = 0; i < len; ++i)
     {
+        // flush if buffer could possibly overflow 
+        if (dataBank->remainingCapacity() < 16)
+        {
+            bank = send(bank);
+            dataBank = &g_dataBanks[bank];
+        }
+
         uint8_t b = bytes[i];
         for (uint8_t mask = 0x80; mask > 0; mask = mask >> 1)
         {
@@ -119,33 +197,36 @@ void write(uint8_t* bytes, uint32_t len)
             }
             
             // Applying preperation state is optional if we're already there
-            if (prepState != *(nextState - 1))
+            if (prepState != lastState)
             {
-                *nextState++ = prepState;
+                dataBank->write(prepState);
             }
 
             // Clock it
-           *nextState++ = clockState;
-        }
-
-        // flush if buffer is about to overflow
-        if ((nextState + 16) > (states + ((sizeof(states) / sizeof(states[0])) - 1)))
-        {
-            nextState = flush(nextState);
+           dataBank->write(clockState);
+           lastState = clockState;
         }
     }
 
+    // flush if buffer could possibly overflow 
+    if (dataBank->remainingCapacity() < 7)
+    {
+        bank = send(bank);
+        dataBank = &g_dataBanks[bank];
+    }
+
     // End
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
-    *nextState++ = SDCKA_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKA_MASK;
-    *nextState++ = 0;
-    *nextState++ = SDCKA_MASK;
-    *nextState++ = (SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
+    dataBank->write(SDCKA_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKA_MASK);
+    dataBank->write(0);
+    dataBank->write(SDCKA_MASK);
+    dataBank->write(SDCKA_MASK | SDCKB_MASK);
 
     // Write and block until done
-    flush(nextState);
+    send(bank);
+    flush();
 }
 
 int main() 
@@ -157,7 +238,24 @@ int main()
 
     systick_hw->rvr = CPU_TICKS_PER_PERIOD - 1;
 
-    uint8_t data[] = {0x00, 0xAA, 0x55, 0xFF, 0x00};
+    gpio_put_all(0);
+    sleep_us(1);
+    gpio_put_all(SDCKA_MASK | SDCKB_MASK);
+
+    uint8_t data[] = {0x0C, 0xAA, 0x55, 0xFF, 
+                      0x00, 0x01, 0x02, 0x03, 
+                      0x04, 0x05, 0x06, 0x07, 
+                      0x08, 0x09, 0x0A, 0x0B, 
+                      0x0C, 0x0D, 0x0E, 0x0F,
+                      0x10, 0x11, 0x12, 0x13, 
+                      0x14, 0x15, 0x16, 0x17, 
+                      0x18, 0x19, 0x1A, 0x1B, 
+                      0x1C, 0x1D, 0x1E, 0x1F,
+                      0x20, 0x21, 0x22, 0x23, 
+                      0x24, 0x25, 0x26, 0x27, 
+                      0x28, 0x29, 0x2A, 0x2B, 
+                      0x2C, 0x2D, 0x2E, 0x2F,
+                      0x00};
     write(data, sizeof(data));
 
     uint8_t data2[] = {0x00, 0x55, 0xFF, 0xAA, 0x00};
