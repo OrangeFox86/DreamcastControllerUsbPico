f98f8369e352d706e1961079394dad3f9c5265bb
James Smith <jmsmith86@gmail.com>
2022-04-18 21:24:01 -0600

    Refactored and managed to achieve 1.7 mbps again

--- MapleBus.cpp
+++ MapleBus.cpp
@@ -8,50 +8,25 @@
 #define SYST_CSR_TICKINT_MASK   0x00000002 // 1 enables isr_systick; 0 disables
 #define SYST_CSR_ENABLE_MASK    0x00000001 // 1 enables counter; 0 disables
 
-// Nominal reload value, not account for any operation adjustment
-#define SYSTICK_RELOAD_VALUE 0x00FFFFFF
-// Nominal threshold, not accounting for any operation adjustment
-#define SYSTICK_NOMINAL_THRESHOLD (SYSTICK_RELOAD_VALUE - CPU_TICKS_PER_PERIOD - 1)
-
 // Mask values for sending A and B bits
 #define MASK_A 0x01
 #define MASK_B 0x02
 #define MASK_AB (MASK_A | MASK_B)
 
-// 11 is approximately the number of operations after the while loop that it takes to write bits and
-// reset cvr
-#define CLOCK_BIT_BIAS 11
-
-static inline void initClock()
-{
-    systick_hw->csr = (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_ENABLE_MASK);
-    systick_hw->rvr = SYSTICK_RELOAD_VALUE;
-}
-
-// This is a bit imprecise, but it gets a better throughput than wasting cycles on an interrupt with
-// all of the delays associated with that
-static inline void clockBit(const uint32_t& mask, const uint32_t& value)
-{
-    uint32_t toggle = (sio_hw->gpio_out ^ value) & mask;
-
-    while(systick_hw->cvr > (SYSTICK_NOMINAL_THRESHOLD + CLOCK_BIT_BIAS));
-    sio_hw->gpio_togl = toggle;
-    systick_hw->cvr = 0;
-}
 
 MapleBus::MapleBus(uint32_t pinA, uint32_t pinB) :
     mPinA(pinA),
     mPinB(pinB),
-    mPinAMask(1 << pinA),
-    mPinBMask(1 << pinB),
-    mAllPinMask(mPinAMask | mPinBMask)
+    mMaskA(1 << pinA),
+    mMaskB(1 << pinB),
+    mMaskAB(mMaskA | mMaskB)
 {
     // B must be the very next output from A
     assert((pinB - pinA) == 1);
     // Initialize the pins as inputs with pull ups so that idle line will always be high
     gpio_init(mPinA);
     gpio_init(mPinB);
-    gpio_set_dir_in_masked(mAllPinMask);
+    gpio_set_dir_in_masked(mMaskAB);
     gpio_set_pulls(mPinA, true, false);
     gpio_set_pulls(mPinB, true, false);
 }
@@ -59,16 +34,17 @@ MapleBus::MapleBus(uint32_t pinA, uint32_t pinB) :
 bool MapleBus::writeInit() const
 {
     // Make sure the clock is turned on
-    initClock();
+    systick_hw->csr = (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_ENABLE_MASK);
+    systick_hw->rvr = SYSTICK_RELOAD_VALUE;
     // Only one pair may be output, so make all others input
-    gpio_set_dir_in_masked(~mAllPinMask);
+    gpio_set_dir_in_masked(~mMaskAB);
     // Ensure no one is pulling low
-    if ((gpio_get_all() & mAllPinMask) == mAllPinMask)
+    if ((gpio_get_all() & mMaskAB) == mMaskAB)
     {
         // Set the two pins at output and keep HIGH
-        gpio_put_all(mAllPinMask);
-        gpio_set_dir_out_masked(mAllPinMask);
-        gpio_put_all(mAllPinMask);
+        gpio_put_all(mMaskAB);
+        gpio_set_dir_out_masked(mMaskAB);
+        gpio_put_all(mMaskAB);
         return true;
     }
     else
@@ -90,31 +66,28 @@ void MapleBus::write(uint8_t* bytes, uint32_t len)
 {
     uint8_t* bytePtr = bytes;
     uint8_t* const endPtr = bytes + len;
-    const uint32_t maskA = MASK_A << mPinA;
-    const uint32_t maskB = MASK_B << mPinA;
-    const uint32_t maskAB = MASK_AB << mPinA;
 
     writeInit();
 
     // Ensure it's at neutral for a few cycles
-    clockBit(maskAB, maskAB);
-    clockBit(maskAB, maskAB);
-    clockBit(maskAB, maskAB);
+    putAB(mMaskAB);
+    putAB(mMaskAB);
+    putAB(mMaskAB);
 
     // Start
-    clockBit(maskAB, maskB);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskB);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskB);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskB);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskB);
-    clockBit(maskAB, maskAB);
+    putAB(mMaskB);
+    putAB(0);
+    putAB(mMaskB);
+    putAB(0);
+    putAB(mMaskB);
+    putAB(0);
+    putAB(mMaskB);
+    putAB(0);
+    putAB(mMaskB);
+    putAB(mMaskAB);
 
     // Data
-    uint32_t lastState = (maskAB);
+    uint32_t lastState = mMaskAB;
     for (; bytePtr < endPtr; ++bytePtr)
     {
         for (uint8_t mask = 0x80; mask != 0; mask = mask >> 1)
@@ -122,21 +95,21 @@ void MapleBus::write(uint8_t* bytes, uint32_t len)
             // A is clock and B is data
             if (*bytePtr & mask)
             {
-                if (lastState != maskAB)
+                if (lastState != mMaskAB)
                 {
-                    clockBit(maskAB, maskAB);
+                    putAB(mMaskAB);
                 }
-                lastState = maskB;
-                clockBit(maskAB, lastState);
+                lastState = mMaskB;
+                putAB(lastState);
             }
             else
             {
-                if (lastState != maskA)
+                if (lastState != mMaskA)
                 {
-                    clockBit(maskAB, maskA);
+                    putAB(mMaskA);
                 }
                 lastState = 0;
-                clockBit(maskAB, lastState);
+                putAB(lastState);
             }
 
             mask = mask >> 1;
@@ -144,33 +117,33 @@ void MapleBus::write(uint8_t* bytes, uint32_t len)
             // B is clock and A is data
             if (*bytePtr & mask)
             {
-                if (lastState != maskAB)
+                if (lastState != mMaskAB)
                 {
-                    clockBit(maskAB, maskAB);
+                    putAB(mMaskAB);
                 }
-                lastState = maskA;
-                clockBit(maskAB, lastState);
+                lastState = mMaskA;
+                putAB(lastState);
             }
             else
             {
-                if (lastState != maskB)
+                if (lastState != mMaskB)
                 {
-                    clockBit(maskAB, maskB);
+                    putAB(mMaskB);
                 }
                 lastState = 0;
-                clockBit(maskAB, lastState);
+                putAB(lastState);
             }
         }
     }
 
     // End
-    clockBit(maskAB, maskAB);
-    clockBit(maskAB, maskA);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskA);
-    clockBit(maskAB, 0);
-    clockBit(maskAB, maskA);
-    clockBit(maskAB, maskAB);
+    putAB(mMaskAB);
+    putAB(mMaskA);
+    putAB(0);
+    putAB(mMaskA);
+    putAB(0);
+    putAB(mMaskA);
+    putAB(mMaskAB);
 
     writeComplete();
 }
--- MapleBus.hpp
+++ MapleBus.hpp
@@ -2,6 +2,8 @@
 #define __MAPLE_BUS_H__
 
 #include "pico/stdlib.h"
+#include "hardware/structs/systick.h"
+#include "configuration.h"
 
 class MapleBus
 {
@@ -10,15 +12,34 @@ class MapleBus
         void write(uint8_t* bytes, uint32_t len);
 
     private:
+
         bool writeInit() const;
         void writeComplete() const;
 
+        // This is a bit imprecise, but it gets a better throughput than wasting cycles on an
+        // interrupt with all of the delays associated with that
+        inline void putAB(const uint32_t& value)
+        {
+            uint32_t toggle = (sio_hw->gpio_out ^ value) & mMaskAB;
+
+            while(systick_hw->cvr > (SYSTICK_NOMINAL_THRESHOLD + CLOCK_BIT_BIAS));
+            sio_hw->gpio_togl = toggle;
+            systick_hw->cvr = 0;
+        }
 
         const uint32_t mPinA;
         const uint32_t mPinB;
-        const uint32_t mPinAMask;
-        const uint32_t mPinBMask;
-        const uint32_t mAllPinMask;
+        const uint32_t mMaskA;
+        const uint32_t mMaskB;
+        const uint32_t mMaskAB;
+
+        // Nominal reload value, not account for any operation adjustment
+        static const uint32_t SYSTICK_RELOAD_VALUE = 0x00FFFFFF;
+        // Nominal threshold, not accounting for any operation adjustment
+        static const uint32_t SYSTICK_NOMINAL_THRESHOLD = (SYSTICK_RELOAD_VALUE - CPU_TICKS_PER_PERIOD - 1);
+        // 11 is approximately the number of operations after the while loop that it takes to write
+        // bits and reset cvr
+        static const uint32_t CLOCK_BIT_BIAS = 11;
 };
 
 #endif // __MAPLE_BUS_H__
