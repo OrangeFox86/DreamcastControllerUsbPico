2781166b5edef2e018e9ba13969e5a774887f993
James Smith <jmsmith86@gmail.com>
2022-04-18 19:54:41 -0600

    Achieved about 1.7 mbps by not using interrupt (1.3 mbps when using interrupt); pulses are not as uniform now, but that is ok

--- configuration.h
+++ configuration.h
@@ -9,9 +9,4 @@
 #define CLOCK_PERIOD_NS 320
 #define CPU_TICKS_PER_PERIOD (int)(CLOCK_PERIOD_NS * CPU_FREQ_MHZ / 1000.0 + 0.5)
 
-// The size of this doesn't represent bits but rather max number of state changes in a bank.
-// Increasing this will increase RAM usage and may increase data encode time but decrease number
-// of data stalls during write; decreasing will do the opposite of all that.
-#define ELEMENTS_PER_BANK 256
-
 #endif // __CONFIGURATION_H__
--- mapleBusClocking.cpp
+++ mapleBusClocking.cpp
@@ -3,7 +3,6 @@
 
 #include "mapleBusClocking.hpp"
 #include "MapleBus.hpp"
-#include "MapleStateBank.hpp"
 #include "configuration.h"
 
 // From datasheet
@@ -11,215 +10,131 @@
 #define SYST_CSR_TICKINT_MASK   0x00000002 // 1 enables isr_systick; 0 disables
 #define SYST_CSR_ENABLE_MASK    0x00000001 // 1 enables counter; 0 disables
 
-// When masking the current bit mask with this, anything other than 0 means A channel is clock
-#define A_IS_CLOCK_MASK 0xAA
+// Nominal reload value, not account for any operation adjustment
+#define SYSTICK_RELOAD_VALUE 0x00FFFFFF
+// Nominal threshold, not accounting for any operation adjustment
+#define SYSTICK_NOMINAL_THRESHOLD (SYSTICK_RELOAD_VALUE - CPU_TICKS_PER_PERIOD - 1)
 
-// Code is tightly coupled to "2" banks, so it makes no sense to have this be anyting but
-#define NUMBER_OF_BANKS 2
+// Mask values for sending A and B bits
+#define MASK_A 0x01
+#define MASK_B 0x02
+#define MASK_AB (MASK_A | MASK_B)
 
-#if (ELEMENTS_PER_BANK < 16)
-   #error "ELEMENTS_PER_BANK must be set to at least 16"
-#endif
+// 13 is approximately the number of operations after the while loop that it takes to write and
+// reset cvr
+#define CLOCK_BIT_BIAS 13
 
-// Note: I believe I can get away without volatile keywords because this buffer is never
-//       read/written to by main code and ISR at the same time. Without volatile, this
-//       speeds up packing time by about 15%.
-MapleStateBank g_dataBanks[NUMBER_OF_BANKS];
-MapleStateBank* g_currentBank = &g_dataBanks[0];
-uint32_t g_nextWrite = 0;
-
-static inline uint32_t is_isr_systick_enabled()
+// This is a bit imprecise, but it gets a better throughput than wasting cycles on an interrupt with
+// all of the delays associated with that
+inline void clockBit(const uint32_t& value)
 {
-    return (systick_hw->csr & SYST_CSR_ENABLE_MASK);
+    while(systick_hw->cvr > (SYSTICK_NOMINAL_THRESHOLD + CLOCK_BIT_BIAS));
+    sio_hw->gpio_out = value;
+    systick_hw->cvr = 0;
 }
 
-static inline void enable_isr_systick()
+// Clock slightly faster for some operations
+#define CLOCK_BIT_FAST_BIAS (CLOCK_BIT_BIAS + 2)
+
+inline void clockBitFast(const uint32_t& value)
 {
-    if (!is_isr_systick_enabled() && !g_currentBank->empty())
-    {
-        g_nextWrite = g_currentBank->readNext();
-        systick_hw->csr = (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_TICKINT_MASK | SYST_CSR_ENABLE_MASK);
-        systick_hw->cvr = 0;
-    }
-}
-
-static inline void disable_isr_systick()
-{
-    systick_hw->csr = 0;
-}
-
-extern "C"
-{
-void isr_systick(void)
-{
-    // Doing this send as very first operation makes for more uniform clocking
-    gpio_put_all(g_nextWrite);
-
-    // I played around with masking, but that would increase bit send time by 15 to 50 ns
-    // Without masking, no other gpio is allowed besides the bus being written to
-    // gpio_put_masked(g_currentBank->getSendMask(), g_nextWrite);
-
-    if (g_currentBank->empty())
-    {
-        disable_isr_systick();
-    }
-    else
-    {
-        g_nextWrite = g_currentBank->readNext();
-    }
-}
-}
-
-static inline void reset_all(void)
-{
-    for (uint32_t i = 0; i < NUMBER_OF_BANKS; ++i)
-    {
-        g_dataBanks[i].reset();
-    }
-    g_currentBank = &g_dataBanks[0];
-}
-
-void flush()
-{
-    enable_isr_systick();
-
-    while (is_isr_systick_enabled());
-
-    disable_isr_systick();
-    g_currentBank->reset();
-}
-
-uint32_t send(uint32_t bankIdx)
-{
-    // Switch to new bank if we aren't already there
-    if (&g_dataBanks[bankIdx] != g_currentBank)
-    {
-        // Wait for current write to complete if it's in the middle of sending
-        if (is_isr_systick_enabled())
-        {
-            while (is_isr_systick_enabled());
-        }
-        // Note: the above check is necessary before checking data within current bank - volatile is
-        //       not used, so read at the same time ISR may be operating on it is not allowed!
-        // Flush anything that is waiting to be sent in the current bank
-        else if (!g_currentBank->empty())
-        {
-            flush();
-        }
-
-        // Switch over to new bank
-        g_currentBank = &g_dataBanks[bankIdx];
-    }
-
-    // Ensure this bank is being sent
-    enable_isr_systick();
-
-    // Return the next bank index
-    if (++bankIdx >= NUMBER_OF_BANKS)
-    {
-        bankIdx = 0;
-    }
-    return bankIdx;
+    while(systick_hw->cvr > (SYSTICK_NOMINAL_THRESHOLD + CLOCK_BIT_FAST_BIAS));
+    sio_hw->gpio_out = value;
+    systick_hw->cvr = 0;
 }
 
 void write(const MapleBus& mapleBus, uint8_t* bytes, uint32_t len)
 {
-    reset_all();
+    uint8_t* bytePtr = bytes;
+    uint8_t* const endPtr = bytes + len;
+    const uint32_t maskA = MASK_A << mapleBus.mPinA;
+    const uint32_t maskB = MASK_B << mapleBus.mPinA;
+    const uint32_t maskAB = MASK_AB << mapleBus.mPinA;
+
     mapleBus.writeInit();
-    uint32_t bankIdx = 0;
-    MapleStateBank* dataBank = &g_dataBanks[bankIdx];
-    dataBank->setMapleBus(mapleBus);
 
     // Ensure it's at neutral for a few cycles
-    dataBank->write(MapleStateBank::MASK_AB);
-    dataBank->write(MapleStateBank::MASK_AB);
-    dataBank->write(MapleStateBank::MASK_AB);
+    clockBit(maskAB);
+    clockBit(maskAB);
+    clockBit(maskAB);
 
-    // Start
-    dataBank->write(MapleStateBank::MASK_B);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_B);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_B);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_B);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_B);
-    dataBank->write(MapleStateBank::MASK_AB);
+    // Start - I think it's good for the start clocks to be slightly faster
+    //         Slower devices that may be close to tolerance will fail to capture this
+    clockBitFast(maskB);
+    clockBitFast(0);
+    clockBitFast(maskB);
+    clockBitFast(0);
+    clockBitFast(maskB);
+    clockBitFast(0);
+    clockBitFast(maskB);
+    clockBitFast(0);
+    clockBitFast(maskB);
+    clockBitFast(maskAB);
 
     // Data
-    uint32_t lastState = (MapleStateBank::MASK_AB);
-    for (uint32_t i = 0; i < len; ++i)
+    uint32_t lastState = (maskAB);
+    for (; bytePtr < endPtr; ++bytePtr)
     {
-        // flush if buffer could possibly overflow
-        if (dataBank->remainingCapacity() < 16)
+        for (uint8_t mask = 0x80; mask != 0; mask = mask >> 1)
         {
-            bankIdx = send(bankIdx);
-            dataBank = &g_dataBanks[bankIdx];
-            dataBank->reset();
-            dataBank->setMapleBus(mapleBus);
-        }
-
-        uint8_t b = bytes[i];
-        for (uint8_t mask = 0x80; mask > 0; mask = mask >> 1)
-        {
-            // Prepare the clock and data lines before clocking it
-            uint32_t prepState = 0;
-            uint32_t clockState = 0;
-            if (mask & A_IS_CLOCK_MASK)
+            // A is clock and B is data
+            if (*bytePtr & mask)
             {
-                // A is clock and B is data
-                if (b & mask)
+                if (lastState != maskAB)
                 {
-                    clockState = MapleStateBank::MASK_B;
+                    clockBit(maskAB);
                 }
-                prepState = MapleStateBank::MASK_A | clockState;
+                lastState = maskB;
+                clockBit(lastState);
             }
             else
             {
-                // B is clock and A is data
-                if (b & mask)
+                if (lastState != maskA)
                 {
-                    clockState = MapleStateBank::MASK_A;
+                    clockBit(maskA);
                 }
-                prepState = MapleStateBank::MASK_B | clockState;
+                lastState = 0;
+                clockBit(lastState);
             }
 
-            // Applying preperation state is optional if we're already there
-            if (prepState != lastState)
+            mask = mask >> 1;
+
+            // B is clock and A is data
+            if (*bytePtr & mask)
             {
-                dataBank->write(prepState);
+                if (lastState != maskAB)
+                {
+                    clockBit(maskAB);
+                }
+                lastState = maskA;
+                clockBit(lastState);
             }
-
-            // Clock it
-           dataBank->write(clockState);
-           lastState = clockState;
+            else
+            {
+                if (lastState != maskB)
+                {
+                    clockBit(maskB);
+                }
+                lastState = 0;
+                clockBit(lastState);
+            }
         }
     }
 
-    // flush if buffer could possibly overflow
-    if (dataBank->remainingCapacity() < 7)
-    {
-        bankIdx = send(bankIdx);
-        dataBank = &g_dataBanks[bankIdx];
-    }
-
     // End
-    dataBank->write(MapleStateBank::MASK_AB);
-    dataBank->write(MapleStateBank::MASK_A);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_A);
-    dataBank->write(0);
-    dataBank->write(MapleStateBank::MASK_A);
-    dataBank->write(MapleStateBank::MASK_AB);
+    clockBit(maskAB);
+    clockBit(maskA);
+    clockBit(0);
+    clockBit(maskA);
+    clockBit(0);
+    clockBit(maskA);
+    clockBit(maskAB);
 
-    // Write and block until done
-    send(bankIdx);
-    flush();
     mapleBus.writeComplete();
 }
 
 void clocking_init()
 {
-    systick_hw->rvr = CPU_TICKS_PER_PERIOD - 1;
+    systick_hw->csr = (SYST_CSR_CLKSOURCE_MASK | SYST_CSR_ENABLE_MASK);
+    systick_hw->rvr = SYSTICK_RELOAD_VALUE;
 }
