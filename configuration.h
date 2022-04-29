90a43a62307c7119d43ea3f39dc23a74672b1b41
James Smith <jmsmith86@gmail.com>
2022-04-28 20:36:39 -0600

    Minor tweaks

--- configuration.h
+++ configuration.h
@@ -4,10 +4,9 @@
 // Adjust the CPU clock frequency here (note: overclocking doesn't help - keep at 133 MHz or less)
 #define CPU_FREQ_MHZ 133
 
-// Adjust the minimum time between each edge here (in tests, timing has a variance of -0/+50 ns)
-// - 300 ns should achieve a throughput between 1.5 and 2 mbps
-// - The Dreamcast achieves an average throughput of about 2 mbps by clocking more efficiently
-#define MIN_CLOCK_PERIOD_NS 300
+// Adjust the minimum time between each edge here
+// 320 ns achieves 2 mbps, just as the dreamcast does
+#define MIN_CLOCK_PERIOD_NS 320
 
 // The minimum amount of time we check for an open line before taking control of it
 // This should be at least as long as the longest clock period of any device on the line.
--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 
 #define CPU_FREQ_KHZ (CPU_FREQ_MHZ * 1000)
 
+#define BUTTON_PIN 2
+
 void core1()
 {
     set_sys_clock_khz(CPU_FREQ_KHZ, true);
@@ -31,42 +33,68 @@ int main()
     gpio_set_dir_out_masked(1<<PICO_DEFAULT_LED_PIN);
     gpio_xor_mask(1<<PICO_DEFAULT_LED_PIN);
 
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir_in_masked(1<<BUTTON_PIN);
+    gpio_set_pulls(BUTTON_PIN, true, false);
+
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &maple_out_program);
     uint sm = pio_claim_unused_sm(pio, true);
     pio_maple_out_init(pio, sm, offset, CPU_FREQ_KHZ, MIN_CLOCK_PERIOD_NS, 14);
     pio_maple_out_start(pio, sm);
 
-    sleep_ms(1000);
-
-    uint32_t dat[] = {0x00010203,0x04050607,0x08090A0B,0x0D0E0F10,0x20304050,0x60708090,0xA0B0C0D0,0xE0F0F1F2,0xF3F4F5F6,0xF7F8F9FA,0xFBFCFDFE,0xFF001122,0x33445566};
-    uint32_t crc = 0;
-    // Condition data (flip byte order) and compute crc
-    for (uint i = 0; i < sizeof(dat)/ sizeof(dat[0]); ++i)
+    while(true)
     {
-        uint32_t word = 0;
-        const uint8_t* src = reinterpret_cast<uint8_t*>(&dat[i]);
-        uint8_t* dst = reinterpret_cast<uint8_t*>(&word) + 3;
-        for (uint j = 0; j < 4; ++j, ++src, --dst)
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+
+        // Wait for button press
+        while (gpio_get(BUTTON_PIN))
         {
-            *dst = *src;
-            crc ^= *src;
+            sleep_ms(25);
         }
-        dat[i] = word;
-    }
-    // The crc byte needs to be shifted to the upper byte of the uint32
-    crc = crc << 24;
 
-    // Send to FIFO!
-    // First word is how many bits are going out
-    pio_sm_put_blocking(pio, sm, sizeof(dat) * 8 + 8);
-    for (uint i = 0; i < sizeof(dat)/ sizeof(dat[0]); ++i)
-    {
-        pio_sm_put_blocking(pio, sm, dat[i]);
-    }
-    pio_sm_put_blocking(pio, sm, crc);
+        gpio_xor_mask(1<<PICO_DEFAULT_LED_PIN);
 
-    gpio_xor_mask(1<<PICO_DEFAULT_LED_PIN);
+        // Wait for button release
+        while (!gpio_get(BUTTON_PIN))
+        {
+            sleep_ms(25);
+        }
+
+        gpio_xor_mask(1<<PICO_DEFAULT_LED_PIN);
+        for (uint i = 0; i < 3; ++i)
+        {
+            sleep_ms(100);
+            gpio_xor_mask(1<<PICO_DEFAULT_LED_PIN);
+        }
+
+        uint32_t dat[] = {0x00010203,0x04050607,0x08090A0B,0x0D0E0F10,0x20304050,0x60708090,0xA0B0C0D0,0xE0F0F1F2,0xF3F4F5F6,0xF7F8F9FA,0xFBFCFDFE,0xFF001122,0x33445566};
+        uint32_t crc = 0;
+        // Condition data (flip byte order) and compute crc
+        for (uint i = 0; i < sizeof(dat)/ sizeof(dat[0]); ++i)
+        {
+            uint32_t word = 0;
+            const uint8_t* src = reinterpret_cast<uint8_t*>(&dat[i]);
+            uint8_t* dst = reinterpret_cast<uint8_t*>(&word) + 3;
+            for (uint j = 0; j < 4; ++j, ++src, --dst)
+            {
+                *dst = *src;
+                crc ^= *src;
+            }
+            dat[i] = word;
+        }
+        // The crc byte needs to be shifted to the upper byte of the uint32
+        crc = crc << 24;
+
+        // Send to FIFO!
+        // First word is how many bits are going out
+        pio_sm_put_blocking(pio, sm, sizeof(dat) * 8 + 8);
+        for (uint i = 0; i < sizeof(dat)/ sizeof(dat[0]); ++i)
+        {
+            pio_sm_put_blocking(pio, sm, dat[i]);
+        }
+        pio_sm_put_blocking(pio, sm, crc);
+    }
 }
 
 
