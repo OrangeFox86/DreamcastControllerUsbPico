a33cd00157495d09f00539f4764983da487acd68
James Smith <jmsmith86@gmail.com>
2022-09-11 13:43:51 -0600

    Corrected some issues with pulsation frequency select in vibration

--- src/coreLib/peripherals/DreamcastVibration.cpp
+++ src/coreLib/peripherals/DreamcastVibration.cpp
@@ -9,7 +9,7 @@ DreamcastVibration::DreamcastVibration(uint8_t addr,
     mTransmissionId(0)
 {
     // Send some vibrations on connection
-    send(7, 0, 200);
+    send(7, 0, 0, 200);
 }
 
 DreamcastVibration::~DreamcastVibration()
@@ -34,7 +34,112 @@ void DreamcastVibration::txComplete(std::shared_ptr<const MaplePacket> packet,
 {
 }
 
-void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination, uint32_t durationMs)
+uint8_t DreamcastVibration::computeNumIncrements(uint8_t power, int8_t inclination)
+{
+    // Compute the number of increments to execute
+    if (inclination > 0)
+    {
+        return (MAX_POWER - power + 1);
+    }
+    else if (inclination < 0)
+    {
+        return  (power - MIN_POWER + 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+uint8_t DreamcastVibration::freqSelect(uint8_t numIncrements, uint8_t pulsation, uint32_t durationMs)
+{
+    pulsation = std::min(pulsation, MAX_PULSATION_VALUE);
+
+    // The adjusted duration value used for checking against limits
+    uint32_t durationValue = (durationMs * numIncrements);
+
+    // Compute the maximum frequency in order to achieve the given duration
+    uint8_t maxFreq = 0;
+    // I didn't spend much time deciding what seems best, but these are good enough
+    if (durationValue < 8000)
+    {
+        maxFreq = MAX_FREQ_VALUE;
+    }
+    else if (durationValue < 12000)
+    {
+        maxFreq = 0x28;
+    }
+    else if (durationValue < 16000)
+    {
+        maxFreq = 0x1D;
+    }
+    else if (durationValue < 20000)
+    {
+        maxFreq = 0x17;
+    }
+    else if (durationValue < 24000)
+    {
+        maxFreq = 0x13;
+    }
+    else if (durationValue < 28000)
+    {
+        maxFreq = 0x10;
+    }
+    else if (durationValue < 33000)
+    {
+        maxFreq = 0x0D;
+    }
+    else if (durationValue < 35000)
+    {
+        maxFreq = 0x0C;
+    }
+    else if (durationValue < 38000)
+    {
+        maxFreq = 0x0B;
+    }
+    else if (durationValue < 41000)
+    {
+        maxFreq = 0x0A;
+    }
+    else if (durationValue < 45000)
+    {
+        maxFreq = 0x09;
+    }
+    else if (durationValue < 50000)
+    {
+        maxFreq = 0x08;
+    }
+    else
+    {
+        maxFreq = MIN_FREQ_VALUE;
+    }
+
+    // Compute the best frequency for the given pulsation value
+    uint8_t freq = maxFreq;
+    if (pulsation != 0)
+    {
+        // If pulsation is set, select the frequency that best gives us that amount of pulsation
+        freq = (MAX_PULSATION_VALUE - pulsation) + MIN_FREQ_VALUE;
+        if (durationValue < 100)
+        {
+            // Can't achieve this short duration with small freq
+            // Not going to be on long enough to feel any pulsation anyway
+            freq = std::max(freq, (uint8_t)0x13);
+        }
+        else if (durationValue < 250)
+        {
+            freq = std::max(freq, (uint8_t)0x08);
+        }
+        else
+        {
+            // Make sure the duration can be achieved
+            freq = std::min(freq, maxFreq);
+        }
+    }
+    return freq;
+}
+
+void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination, uint8_t pulsation, uint32_t durationMs)
 {
     // This is just a guesstimate of what is going on here based on trial and error...
 
@@ -60,7 +165,10 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
     //         - Most sig nibble must be 1 for the command to be accepted (unless entire word is 0x00000000)
     //         - Least sig nibble must be 0 or 1 for the command to be accepted
     //         - The least significant nibble when set to 1: override to internal duration
-    // A value of 0x00000000 will stop current vibration
+    // A value of 0x10000000 will stop current vibration
+
+    // TODO: There is a block write command which sets the maximum vibration time. That is also what
+    //       is used when byte 3 least sig nibble is set to 1. Implement that value somewhere.
 
     uint32_t vibrationWord = 0x10000000;
 
@@ -68,58 +176,38 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
     {
         // Limit power value to 7
         power = std::min(power, (uint8_t)MAX_POWER);
-        uint8_t numPowerSteps = 0;
+
+        // Compute number of increments and frequency
+        uint8_t numIncrements = computeNumIncrements(power, inclination);
+        uint8_t freq = freqSelect(numIncrements, pulsation, durationMs);
+
         // Set Power and inclination direction into word
         if (inclination > 0)
         {
             vibrationWord |= (power << 20) | 0x080000;
-            numPowerSteps = (MAX_POWER - power + 1);
         }
         else if (inclination < 0)
         {
             vibrationWord |= (power << 16) | 0x800000;
-            numPowerSteps = (MAX_POWER - power + 1);
             // Force inclination to be positive for the next step
             inclination = -inclination;
         }
         else
         {
             vibrationWord |= (power << 20);
-            numPowerSteps = 1;
         }
 
-        // Determine frequency and inclination
-        uint32_t freq = 0;
-        // Select a frequency bin to use
-        // I didn't spend much time deciding what seems best, but these are good enough
-        if ((durationMs * numPowerSteps) > 30000)
-        {
-            freq = MIN_FREQ_VALUE;
-        }
-        else if ((durationMs * numPowerSteps) > 10000)
-        {
-            freq = 0x0F;
-        }
-        else if ((durationMs * numPowerSteps) > 5000)
-        {
-            freq = 0x28;
-        }
-        else
-        {
-            freq = MAX_FREQ_VALUE;
-        }
         // I generated this equation based on observed vibration times
         // (it's close but not perfect - off by 5% to 10%)
-        uint32_t inclinationValue = std::min(
-            (uint32_t)(((freq / 2 + 1) * (durationMs / 1000.0)) / numPowerSteps),
-            MAX_INCLINATION);
+        uint32_t durationValue = std::min(
+            (uint32_t)(((freq / 2 + 1) * (durationMs / 1000.0)) / numIncrements),
+            MAX_DURATION_VALUE);
 
-        // Set frequency and inclination
-        vibrationWord |= (freq << 8) | inclinationValue;
+        // Set frequency and duration
+        vibrationWord |= (freq << 8) | durationValue;
     }
 
     // Send it!
-    //DEBUG_PRINT("%08lX\n", vibrationWord);
     uint32_t payload[2] = {FUNCTION_CODE, vibrationWord};
     mEndpointTxScheduler->add(
         timeUs,
@@ -131,7 +219,12 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
         0);
 }
 
-void DreamcastVibration::send(uint8_t power, int8_t inclination, uint32_t durationMs)
+void DreamcastVibration::send(uint8_t power, int8_t inclination, uint8_t pulsation, uint32_t durationMs)
 {
-    send(PrioritizedTxScheduler::TX_TIME_ASAP, power, inclination, durationMs);
+    send(PrioritizedTxScheduler::TX_TIME_ASAP, power, inclination, pulsation, durationMs);
+}
+
+void DreamcastVibration::stop()
+{
+    send(0, 0, 0, 0);
 }
--- src/coreLib/peripherals/DreamcastVibration.hpp
+++ src/coreLib/peripherals/DreamcastVibration.hpp
@@ -38,17 +38,36 @@ class DreamcastVibration : public DreamcastPeripheral
         //! @param[in] timeUs  The time to send vibration (optional)
         //! @param[in] power  Starting power intensity [0,7] (0 to disable completely)
         //! @param[in] inclination  -1: ramp down, 0: constant, 1: ramp up
+        //! @param[in] pulsation  The amount of pulsation to apply [0, 32] (0 for none, 32 for max)
         //! @param[in] durationMs  The length of time in ms to vibrate (0 to disable completely)
-        void send(uint64_t timeUs, uint8_t power, int8_t inclination, uint32_t durationMs);
-        void send(uint8_t power, int8_t inclination, uint32_t durationMs);
+        void send(uint64_t timeUs, uint8_t power, int8_t inclination, uint8_t pulsation, uint32_t durationMs);
+        void send(uint8_t power, int8_t inclination, uint8_t pulsation, uint32_t durationMs);
+
+        //! Stops current vibration
+        void stop();
+
+    private:
+        //! Computes the number of power increments that will be executed
+        //! @param[in] power  Starting power intensity [1,7]
+        //! @param[in] inclination  -1: ramp down, 0: constant, 1: ramp up
+        //! @returns the number of power increments
+        uint8_t computeNumIncrements(uint8_t power, int8_t inclination);
+
+        //! Selects the frequency best suited to achieve the given parameters
+        //! @param[in] numIncrements  The number of power increments that will be executed
+        //! @param[in] pulsation  The amount of pulsation to apply [0, 32] (0 for none, 32 for max)
+        //! @returns the selected frequency value
+        uint8_t freqSelect(uint8_t numIncrements, uint8_t pulsation, uint32_t durationMs);
 
     public:
         //! Function code for screen
         static const uint32_t FUNCTION_CODE = DEVICE_FN_VIBRATION;
         static const uint32_t MAX_FREQ_VALUE = 0x3B;
         static const uint32_t MIN_FREQ_VALUE = 0x07;
-        static const uint32_t MAX_INCLINATION = 0x255;
+        static const uint32_t MAX_DURATION_VALUE = 0xFF;
         static const uint32_t MAX_POWER = 0x07;
+        static const uint32_t MIN_POWER = 0x01;
+        static const uint8_t MAX_PULSATION_VALUE = 32;
 
     private:
         uint32_t mTransmissionId;
