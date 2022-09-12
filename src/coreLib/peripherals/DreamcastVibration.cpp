68dd2ba4eb3f5d4d5ce24a9a4c9e79f929cffbc6
James Smith <jmsmith86@gmail.com>
2022-09-11 19:12:05 -0600

    Cleaned up vibration peripheral - block read and write are not supported

--- src/coreLib/peripherals/DreamcastVibration.cpp
+++ src/coreLib/peripherals/DreamcastVibration.cpp
@@ -4,29 +4,35 @@
 
 // Duration value for given increments, frequency, and duration
 // I generated this equation based on observed vibration times with a test device
-#define COMPUTE_DURATION_VALUE(numIncrements, freq, durationMs) (uint32_t)(((freq / 2.0 + 1) * (durationMs / 1000.0)) / numIncrements) - 1
+#define COMPUTE_DURATION_VALUE(numIncrements, freq, durationMs) (                       \
+    (uint8_t)std::max(                                                                  \
+        std::min(                                                                       \
+            (int32_t)(((freq / 2.0 + 1) * (durationMs / 1000.0)) / numIncrements) - 1,  \
+            (int32_t)MAX_DURATION_VALUE),                                               \
+        (int32_t)0)                                                                     \
+)
 #define FREQ_IDX_TO_FREQ(freqIdx) (freqIdx + MIN_FREQ_VALUE)
 #define FREQ_TO_FREQ_IDX(freq) (freq - MIN_FREQ_VALUE)
 // Maximum duration for a single increment
-#define COMPUTE_MAX_DURATION(freqIdx) (uint32_t)(1000 * (MAX_DURATION_VALUE + 1) / (FREQ_IDX_TO_FREQ(freqIdx) / 2.0 + 1))
+#define COMPUTE_MAX_DURATION_MS(freqIdx) (uint32_t)(1000 * (MAX_DURATION_VALUE + 1) / (FREQ_IDX_TO_FREQ(freqIdx) / 2.0 + 1))
 
 // There are ways of generating this like using BOOST_PP_ENUM, but I don't want to include boost just for that
 const uint32_t DreamcastVibration::MAX_DURATION_LOOKUP[NUM_FREQ_VALUES] =
 {
-    COMPUTE_MAX_DURATION(0), COMPUTE_MAX_DURATION(1), COMPUTE_MAX_DURATION(2), COMPUTE_MAX_DURATION(3),
-    COMPUTE_MAX_DURATION(4), COMPUTE_MAX_DURATION(5), COMPUTE_MAX_DURATION(6), COMPUTE_MAX_DURATION(7),
-    COMPUTE_MAX_DURATION(8), COMPUTE_MAX_DURATION(9), COMPUTE_MAX_DURATION(10), COMPUTE_MAX_DURATION(11),
-    COMPUTE_MAX_DURATION(12), COMPUTE_MAX_DURATION(13), COMPUTE_MAX_DURATION(14), COMPUTE_MAX_DURATION(15),
-    COMPUTE_MAX_DURATION(16), COMPUTE_MAX_DURATION(17), COMPUTE_MAX_DURATION(18), COMPUTE_MAX_DURATION(19),
-    COMPUTE_MAX_DURATION(20), COMPUTE_MAX_DURATION(21), COMPUTE_MAX_DURATION(22), COMPUTE_MAX_DURATION(23),
-    COMPUTE_MAX_DURATION(24), COMPUTE_MAX_DURATION(25), COMPUTE_MAX_DURATION(26), COMPUTE_MAX_DURATION(27),
-    COMPUTE_MAX_DURATION(28), COMPUTE_MAX_DURATION(29), COMPUTE_MAX_DURATION(30), COMPUTE_MAX_DURATION(31),
-    COMPUTE_MAX_DURATION(32), COMPUTE_MAX_DURATION(33), COMPUTE_MAX_DURATION(34), COMPUTE_MAX_DURATION(35),
-    COMPUTE_MAX_DURATION(36), COMPUTE_MAX_DURATION(37), COMPUTE_MAX_DURATION(38), COMPUTE_MAX_DURATION(39),
-    COMPUTE_MAX_DURATION(40), COMPUTE_MAX_DURATION(41), COMPUTE_MAX_DURATION(42), COMPUTE_MAX_DURATION(43),
-    COMPUTE_MAX_DURATION(44), COMPUTE_MAX_DURATION(45), COMPUTE_MAX_DURATION(46), COMPUTE_MAX_DURATION(47),
-    COMPUTE_MAX_DURATION(48), COMPUTE_MAX_DURATION(49), COMPUTE_MAX_DURATION(50), COMPUTE_MAX_DURATION(51),
-    COMPUTE_MAX_DURATION(52)
+    COMPUTE_MAX_DURATION_MS(0),  COMPUTE_MAX_DURATION_MS(1),  COMPUTE_MAX_DURATION_MS(2),  COMPUTE_MAX_DURATION_MS(3),
+    COMPUTE_MAX_DURATION_MS(4),  COMPUTE_MAX_DURATION_MS(5),  COMPUTE_MAX_DURATION_MS(6),  COMPUTE_MAX_DURATION_MS(7),
+    COMPUTE_MAX_DURATION_MS(8),  COMPUTE_MAX_DURATION_MS(9),  COMPUTE_MAX_DURATION_MS(10), COMPUTE_MAX_DURATION_MS(11),
+    COMPUTE_MAX_DURATION_MS(12), COMPUTE_MAX_DURATION_MS(13), COMPUTE_MAX_DURATION_MS(14), COMPUTE_MAX_DURATION_MS(15),
+    COMPUTE_MAX_DURATION_MS(16), COMPUTE_MAX_DURATION_MS(17), COMPUTE_MAX_DURATION_MS(18), COMPUTE_MAX_DURATION_MS(19),
+    COMPUTE_MAX_DURATION_MS(20), COMPUTE_MAX_DURATION_MS(21), COMPUTE_MAX_DURATION_MS(22), COMPUTE_MAX_DURATION_MS(23),
+    COMPUTE_MAX_DURATION_MS(24), COMPUTE_MAX_DURATION_MS(25), COMPUTE_MAX_DURATION_MS(26), COMPUTE_MAX_DURATION_MS(27),
+    COMPUTE_MAX_DURATION_MS(28), COMPUTE_MAX_DURATION_MS(29), COMPUTE_MAX_DURATION_MS(30), COMPUTE_MAX_DURATION_MS(31),
+    COMPUTE_MAX_DURATION_MS(32), COMPUTE_MAX_DURATION_MS(33), COMPUTE_MAX_DURATION_MS(34), COMPUTE_MAX_DURATION_MS(35),
+    COMPUTE_MAX_DURATION_MS(36), COMPUTE_MAX_DURATION_MS(37), COMPUTE_MAX_DURATION_MS(38), COMPUTE_MAX_DURATION_MS(39),
+    COMPUTE_MAX_DURATION_MS(40), COMPUTE_MAX_DURATION_MS(41), COMPUTE_MAX_DURATION_MS(42), COMPUTE_MAX_DURATION_MS(43),
+    COMPUTE_MAX_DURATION_MS(44), COMPUTE_MAX_DURATION_MS(45), COMPUTE_MAX_DURATION_MS(46), COMPUTE_MAX_DURATION_MS(47),
+    COMPUTE_MAX_DURATION_MS(48), COMPUTE_MAX_DURATION_MS(49), COMPUTE_MAX_DURATION_MS(50), COMPUTE_MAX_DURATION_MS(51),
+    COMPUTE_MAX_DURATION_MS(52)
 };
 
 DreamcastVibration::DreamcastVibration(uint8_t addr,
@@ -146,14 +152,8 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
     // Byte 3: 10 or 11 ???
     //         - Most sig nibble must be 1 for the command to be accepted (unless entire word is 0x00000000)
     //         - Least sig nibble must be 0 or 1 for the command to be accepted
-    //         - The least significant nibble when set to 1: override to internal duration
+    //         - The least significant nibble when set to 1: augments duration
     // A value of 0x10000000 will stop current vibration
-
-    // TODO: There is a block write command which sets the maximum vibration time. That is also what
-    //       is used when byte 3 least sig nibble is set to 1. Implement that value somewhere. That
-    //       value may be useful for:
-    //       - Getting long vibration duration without pulsation
-    //       - Ramp and stop short of max/min without worrying about scheduling a stop event
 
     uint32_t vibrationWord = 0x10000000;
 
@@ -174,8 +174,6 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
         else if (inclination < 0)
         {
             vibrationWord |= (power << 16) | 0x800000;
-            // Force inclination to be positive for the next step
-            inclination = -inclination;
         }
         else
         {
@@ -183,9 +181,7 @@ void DreamcastVibration::send(uint64_t timeUs, uint8_t power, int8_t inclination
         }
 
         // Compute duration and limit to max value
-        uint32_t durationValue = std::min(
-            COMPUTE_DURATION_VALUE(numIncrements, freq, durationMs),
-            MAX_DURATION_VALUE);
+        uint8_t durationValue = COMPUTE_DURATION_VALUE(numIncrements, freq, durationMs);
 
         // Set frequency and duration
         vibrationWord |= (freq << 8) | durationValue;
