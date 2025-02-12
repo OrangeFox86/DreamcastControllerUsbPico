5cc9077c656e682f4c7fdba4075d03494844e074
James Smith <{ID}+{username}@users.noreply.github.com>
2025-02-11 21:42:49 -0700

    156: Just define SERIAL_SIZE in-place

--- inc/hal/System/Identification.hpp
+++ inc/hal/System/Identification.hpp
@@ -1,6 +1,6 @@
 #include <cstdint>
 
 // size includes null terminator
-extern const std::uint32_t SERIAL_SIZE;
+#define SERIAL_SIZE 17
 
 void system_get_serial(char* buffer, std::uint32_t bufflen);
--- src/hal/System/Identification.cpp
+++ src/hal/System/Identification.cpp
@@ -1,8 +1,6 @@
 #include <hal/System/Identification.hpp>
 #include "pico/unique_id.h"
 
-const std::uint32_t SERIAL_SIZE = (PICO_UNIQUE_BOARD_ID_SIZE_BYTES * 2 + 1);
-
 void system_get_serial(char* buffer, std::uint32_t bufflen)
 {
     pico_get_unique_board_id_string(buffer, bufflen);
